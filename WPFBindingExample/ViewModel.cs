c6dfb1c0ec8e1d15650db16fd35215d262e8be7d
ArchyInUse <archyinuse@gmail.com>
2020-01-24 13:12:08 +0200

    initial

---
+++ WPFBindingExample/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFBindingExample
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string text;
+        public string VmText
+        {
+            get => text;
+            set
+            {
+                text = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
